b959f6864fee1ab2a8bc91a45ee4d61e941e0c34
John Restrepo <jfrestre@gmail.com>
2019-11-01 11:45:59 -0500

    Refactoring of code.
    Extended methods of class Square and Piece.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 # Pychess
-import itertools
+
+
+# Global variables
+FILES = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+RANKS = ['1', '2', '3', '4', '5', '6', '7', '8']
 
 
 class Square:
@@ -17,6 +21,9 @@ class Square:
 
     def __repr__(self):
         return " ".join([self.file, self.rank])
+
+    def __eq__(self, other):
+        return (self.file == other.file) and (self.rank == other.rank)
 
 
 class Piece:
@@ -42,13 +49,31 @@ class Piece:
     def change_position(self, new_square):
         self.square = new_square
 
+    def valid_squares(self):
+        """
+        Compute all valid squares for the current piece as if it were alone in the board.
+        :return: List of Positions objects
+        """
+        # Rooks
+        types = {'R': Piece.valid_rook}
+
+        def select_function(function, piece):
+            return function(piece)
+
+        squares = select_function(types.get(self.name), self.square)
+        return squares
+
+    @staticmethod
+    def valid_rook(square):
+        horizontal = {file + square.rank for file in FILES}
+        vertical = {square.file + rank for rank in RANKS}
+        coordinates = sorted(horizontal ^ vertical)                 # symmetric difference
+        return [Square(coord) for coord in coordinates]
 
 class Chess:
     """
     Class for all game management
     """
-    _files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    _ranks = ['1', '2', '3', '4', '5', '6', '7', '8']
     _valuations = {'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'K': 0}
 
     def __init__(self, setup):
@@ -61,11 +86,10 @@ class Chess:
         self.dead_white = []
         self.dead_black = []
         self.score = 0
-        self.board = []
 
         if setup.lower() == 'default':
             # Pawns
-            for file in Chess._files:
+            for file in FILES:
                 self.active_white.append(Piece('P', 'w', True, Square(file + '2')))
                 self.active_black.append(Piece('P', 'b', True, Square(file + '7')))
 
@@ -96,35 +120,76 @@ class Chess:
             self.active_black.append(Piece('K', 'b', True, Square('e8')))
 
         elif setup.lower() == "simple":
-            self.active_white.append(Piece('P', 'w', True, 'e2'))
-            self.active_white.append(Piece('P', 'w', False, 'e2'))
-            self.active_white.append(Piece('P', 'w', False, 'e2'))
-            self.active_white.append(Piece('P', 'w', False, 'e2'))
+            self.active_white.append(Piece('R', 'w', True, 'f7'))
+            self.active_white, self.dead_white = Chess.check_active(self.active_white, self.dead_white)
 
     def print(self):
         """
         Primitive print of the board to console
         :return:
         """
-        self.board = [["" for _ in range(8)] for __ in range(8)]
+        board = [["" for _ in range(8)] for __ in range(8)]
 
-        all_active = [self.active_white, self.active_black]
-        for piece in itertools.chain(*all_active):
+        all_active = self.active_white + self.active_black
+        for piece in all_active:
             col = ord(piece.square.file) - ord('a')
             row = int(piece.square.rank) - 1
-            self.board[row][col] = piece.name + piece.color
+            board[row][col] = piece.name + piece.color
 
         line = " " + "-- " * 8 + "\n"
         board_string = line
         for r in range(8):
             for c in range(8):
-                if self.board[r][c] != "":
-                    board_string += "|" + self.board[r][c]
+                if board[r][c] != "":
+                    board_string += "|" + board[r][c]
                 else:
                     board_string += "|" + "  "
             board_string += "|\n" + line
 
         print(board_string)
+
+
+    def print_valid(self, square):
+        """
+        Print valid positions for a piece in the specified square
+        :param square:
+        :return: None
+        """
+        all_active = self.active_white + self.active_black
+
+        selected_piece = None
+        for piece in all_active:
+            if piece.square == square:
+                selected_piece = piece
+
+        if selected_piece is None:
+            print("No piece in the selected square")
+            exit()
+
+        board = [["" for _ in range(8)] for __ in range(8)]
+
+        for piece in all_active:
+            col = ord(piece.square.file) - ord('a')
+            row = int(piece.square.rank) - 1
+            board[row][col] = piece.name + piece.color
+
+        for square in selected_piece.valid_squares():
+            col = ord(square.file) - ord('a')
+            row = int(square.rank) - 1
+            board[row][col] = "++"
+
+        line = " " + "-- " * 8 + "\n"
+        board_string = line
+        for r in range(8):
+            for c in range(8):
+                if board[r][c] != "":
+                    board_string += "|" + board[r][c]
+                else:
+                    board_string += "|" + "  "
+            board_string += "|\n" + line
+
+        print(board_string)
+
 
     def game_score(self):
         """
@@ -132,8 +197,8 @@ class Chess:
         :return:
         score: signed integer
         """
-        all_active = [self.active_white, self.active_black]
-        for piece in itertools.chain(*all_active):
+        all_active = self.active_white + self.active_black
+        for piece in all_active:
             if piece.state:
                 if piece.color == 'w':
                     self.score += Chess._valuations[piece.name]
@@ -144,93 +209,126 @@ class Chess:
     def set_current_player(self, new_player):
         self.current_player = new_player
 
-    def move(self, entry):
-        current_active = []
-        if self.current_player == 'w':
-            current_active = self.active_white
-        elif self.current_player == 'b':
-            current_active = self.active_black
+    # def parse_move(self, entry):
+    #     """
+    #     Transform the chess move from regular notation to a tuple having the
+    #     initial and last positions. Check for validity of notation.
+    #     :param entry: string with move
+    #     :return: Tuple with Position objects (old, new)
+    #     """
+    #
+    #     # Detect if move has correct notation
+    #     assert(1 < len(entry) < 6, "Move has incorrect length of characters")
+    #
+    #     # Obtain active player pieces
+    #     if self.current_player == 'w':
+    #         current_active = self.active_white
+    #     else:
+    #         current_active = self.active_black
+    #
+    #     #   Detect what type of piece is moving
+    #     if (entry[0] in Chess.FILES) and (len(entry) == 2):        # Simple pawn move
+    #         old = Square(entry[0] + chr(ord(entry[1])-1))
+    #         new = Square(entry)
+    #     elif (entry[0] in Chess._valuations.keys()) and (len(entry) == 3):      #Simple mayor piece move
+    #         selected_pieces = [piece for piece in current_active if piece.name == entry[0]]
+    #
+    #
+    #     # Detect location of piece
+    #
+    #     # Detect if move is legal
+    #     # Translate to tuple of Positions
+    #
+    #     return (old, new)
 
-        # Pawns move
-        selected_pieces = []  # Select possible pieces to move
-        if len(entry) == 2:
-            for piece in current_active:
-                if (piece.name == 'P') and (piece.square.file == entry[0]):
-                    selected_pieces.append(piece)
+    # def move(self, entry):
+    #     current_active = []
+    #     if self.current_player == 'w':
+    #         current_active = self.active_white
+    #     elif self.current_player == 'b':
+    #         current_active = self.active_black
+    #
+    #     # Pawns move
+    #     selected_pieces = []  # Select possible pieces to move
+    #     if len(entry) == 2:
+    #         for piece in current_active:
+    #             if (piece.name == 'P') and (piece.square.file == entry[0]):
+    #                 selected_pieces.append(piece)
+    #
+    #     if len(selected_pieces) == 1:
+    #         if selected_pieces[0].square.rank in ['2', '7']:
+    #             if (self.current_player == 'w') and (0 < ord(entry[1]) - ord(selected_pieces[0].square.rank) < 3):
+    #                 selected_pieces[0].square = Square(entry)
+    #             elif (self.current_player == 'b') and (0 < ord(selected_pieces[0].square.rank) - ord(entry[1]) < 3):
+    #                 selected_pieces[0].square = Square(entry)
+    #             else:
+    #                 print("Invalid position")
+    #                 exit()
+    #         else:
+    #             if (self.current_player == 'w') and (ord(entry[1]) - ord(selected_pieces[0].square.rank) == 1):
+    #                 selected_pieces[0].square = Square(entry)
+    #             elif (self.current_player == 'b') and (ord(selected_pieces[0].square.rank) - ord(entry[1]) == 1):
+    #                 selected_pieces[0].square = Square(entry)
+    #             else:
+    #                 print("Invalid position")
+    #                 exit()
 
-        if len(selected_pieces) == 1:
-            if selected_pieces[0].square.rank in ['2', '7']:
-                if (self.current_player == 'w') and (0 < ord(entry[1]) - ord(selected_pieces[0].square.rank) < 3):
-                    selected_pieces[0].square = Square(entry)
-                elif (self.current_player == 'b') and (0 < ord(selected_pieces[0].square.rank) - ord(entry[1]) < 3):
-                    selected_pieces[0].square = Square(entry)
-                else:
-                    print("Invalid position")
-                    exit()
+    #
+    # selected_pieces = []  # Select possible pieces to move
+    # for piece in current_active:
+    #     if entry[0] == piece.name:
+    #         selected_pieces.append(piece)
+    #
+    # new_position = entry[1:]
+    # if len(selected_pieces) == 1:
+    # Rooks
+    # if (selected_pieces[0].position[0] == new_position[0]) or (
+    #         selected_pieces[0].position[1] == new_position[1]):
+    #     selected_pieces[0].position = new_position
+    # else:
+    #     print("Invalid position!")
+    #     exit()
+
+    # Bishops
+    # if abs(ord(selected_pieces[0].position[0]) - ord(new_position[0])) == abs(
+    #         ord(selected_pieces[0].position[1]) - ord(new_position[1])):
+    #     selected_pieces[0].position = new_position
+    # else:
+    #     print("Invalid position!")
+    #     exit()
+
+    # Knights
+    # if (abs(ord(selected_pieces[0].position[0]) - ord(new_position[0])) == 2) and (abs(ord(selected_pieces[0].position[1]) - ord(new_position[1])) == 1):
+    #     selected_pieces[0].position = new_position
+    # elif (abs(ord(selected_pieces[0].position[0]) - ord(new_position[0])) == 1) and (abs(ord(selected_pieces[0].position[1]) - ord(new_position[1])) == 2):
+    #     selected_pieces[0].position = new_position
+    # else:
+    #     print("Invalid position")
+    #     exit()
+
+    # Queens
+    # if (selected_pieces[0].position[0] == new_position[0]) or (
+    #         selected_pieces[0].position[1] == new_position[1]) or (
+    #         abs(ord(selected_pieces[0].position[0]) - ord(new_position[0])) == abs(
+    #         ord(selected_pieces[0].position[1]) - ord(new_position[1]))):
+    #     selected_pieces[0].position = new_position
+    # else:
+    #     print("Invalid position!")
+    #     exit()
+
+    @staticmethod
+    def check_active(active, dead):
+        new_active = []
+        for piece in active:
+            if not piece.state:
+                dead.append(piece)
             else:
-                if (self.current_player == 'w') and (ord(entry[1]) - ord(selected_pieces[0].square.rank) == 1):
-                    selected_pieces[0].square = Square(entry)
-                elif (self.current_player == 'b') and (ord(selected_pieces[0].square.rank) - ord(entry[1]) == 1):
-                    selected_pieces[0].square = Square(entry)
-                else:
-                    print("Invalid position")
-                    exit()
-
-        #
-        # selected_pieces = []  # Select possible pieces to move
-        # for piece in current_active:
-        #     if entry[0] == piece.name:
-        #         selected_pieces.append(piece)
-        #
-        # new_position = entry[1:]
-        # if len(selected_pieces) == 1:
-        # Rooks
-        # if (selected_pieces[0].position[0] == new_position[0]) or (
-        #         selected_pieces[0].position[1] == new_position[1]):
-        #     selected_pieces[0].position = new_position
-        # else:
-        #     print("Invalid position!")
-        #     exit()
-
-        # Bishops
-        # if abs(ord(selected_pieces[0].position[0]) - ord(new_position[0])) == abs(
-        #         ord(selected_pieces[0].position[1]) - ord(new_position[1])):
-        #     selected_pieces[0].position = new_position
-        # else:
-        #     print("Invalid position!")
-        #     exit()
-
-        # Knights
-        # if (abs(ord(selected_pieces[0].position[0]) - ord(new_position[0])) == 2) and (abs(ord(selected_pieces[0].position[1]) - ord(new_position[1])) == 1):
-        #     selected_pieces[0].position = new_position
-        # elif (abs(ord(selected_pieces[0].position[0]) - ord(new_position[0])) == 1) and (abs(ord(selected_pieces[0].position[1]) - ord(new_position[1])) == 2):
-        #     selected_pieces[0].position = new_position
-        # else:
-        #     print("Invalid position")
-        #     exit()
-
-        # Queens
-        # if (selected_pieces[0].position[0] == new_position[0]) or (
-        #         selected_pieces[0].position[1] == new_position[1]) or (
-        #         abs(ord(selected_pieces[0].position[0]) - ord(new_position[0])) == abs(
-        #         ord(selected_pieces[0].position[1]) - ord(new_position[1]))):
-        #     selected_pieces[0].position = new_position
-        # else:
-        #     print("Invalid position!")
-        #     exit()
-
-    # @staticmethod
-    # def check_active(active, dead):
-    #     for itr, piece in enumerate(active):
-    #         if not piece.state:
-    #             dead.append(active.pop(itr))
-    #     return dead
+                new_active.append(piece)
+        return new_active, dead
 
 
 c = Chess("simple")
 # print(c.board['f']['5'])
 c.print()
 
-c.move('e4')
-
-c.print()
+c.print_valid(Square("f7"))
